5c236c9200d052a63bac98d6c4b552ab92ba57c1
Nelly <nkhmeleva@yahoo.com>
2020-01-12 01:51:55 -0700

    removed port

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.use('/auth', require('./routes/auth'))
 app.use('/profile', require('./routes/profile'))
 // app.use('/posts', require('./routes/posts'))
 
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
