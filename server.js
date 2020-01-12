6de14ccc5c9452a70876c5c5d34bde4d198f9757
Nelly <nkhmeleva@yahoo.com>
2020-01-12 11:08:11 -0700

    prep for heroku2

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const connectDB = require('./config/db')
-
+const cors = require('cors')
 const app = express()
 
 connectDB()
 
-app.use(express.json({extended: false}))
+app.use(express.json({extended: false}), cors(corsOptions))
 
-app.get('/', (req,res) => res.send("API running"))
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
 //routes
 app.use('/users', require('./routes/users'))
 app.use('/auth', require('./routes/auth'))
 app.use('/profile', require('./routes/profile'))
 // app.use('/posts', require('./routes/posts'))
+
+
 
 const PORT = process.env.PORT || 5000
 
