90a236ed0ff3e86b6482029a98efa4c869faf9de
Nelly <nkhmeleva@yahoo.com>
2020-01-12 11:49:02 -0700

    prep for heroku6

--- server.js
+++ server.js
@@ -18,14 +18,8 @@ app.use('/profile', require('./routes/profile'))
 // app.use('/posts', require('./routes/posts'))
 
 
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve('index.html'));
-    });
-  }
+process.env.NODE_ENV === 'production'
+ 
   
   const PORT = process.env.PORT || 5000;
   
